9c93946b39d4e42f95cd8fc4c9c357ffb94d5b73
saepulid <saepull.id@gmail.com>
2024-06-29 14:09:47 +0700

    update: create repository

---
+++ api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	app *gin.Engine
+)
+
+func myRoutes(r *gin.RouterGroup) {
+	r.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "It Works !, Hello World !"})
+	})
+}
+
+func init() {
+	app = gin.New()
+	r := app.Group("/api")
+	myRoutes(r)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
